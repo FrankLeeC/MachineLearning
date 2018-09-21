7aa1163639e0bb1ffb3ccdaeaa513c9090a93c12
frank lee <frank@liwanyi.me>
2018-09-21 11:11:18 +0100

    gba

--- rl/bendit.py
+++ rl/bendit.py
@@ -11,6 +11,7 @@ class Arm:
         self.real_p = real_p
         self.estimate_p = 0.0
         self.n = 0
+        self.gba = 0.0
 
     def get_used_count(self):
         return self.n
@@ -21,6 +22,18 @@ class Arm:
     def update(self, reward):
         self.n += 1
         self.estimate_p += (reward - self.estimate_p)/self.n
+
+    def get_gba(self):
+        return self.gba
+
+    def update_gba(self, p, reward, alpha):
+        '''
+        当前 arm 是被选择的
+        alpha > 0 p = 1-PI(A_t)
+        否则
+        alpha < 0 p = PI(A_t)
+        '''
+        self.gba += alpha*(reward-self.estimate_p)*p
 
 
 class Bendit:
@@ -62,8 +75,22 @@ class Bendit:
                 if e.get_used_count() == 0:
                     return i, int(i == self.best())
                 score.append(e.get_p() + c*np.sqrt(np.log(self.select_count)/e.get_used_count())) 
-            b = np.argmax(score)
-            return b, int(b == self.best())
+            idx = np.argmax(score)
+            return idx, int(idx == self.best())
+        elif select_method == 'gba':
+            gba_list = [e.get_gba() for e in self.arm_list]
+            p = softmax(gba_list)
+            self.p = p
+            idx = np.argmax(p)
+            return idx, int(idx == self.best())
+
+    def update_gba(self, idx, reward):
+        alpha = 0.1
+        for i, e in enumerate(self.p):
+            if i != idx:
+                self.arm_list[i].update_gba(self.p[i], reward, -alpha)
+            else:
+                self.arm_list[i].update_gba(1-self.p[i], reward, alpha)
 
     def best(self):
         return np.argmax([a.real_p for a in self.arm_list])
@@ -73,6 +100,9 @@ class Bendit:
         self.arm_list[arm_idx].update(reward)
         return reward
 
+def softmax(a):
+    a = np.exp(a)
+    return a/np.sum(a)
 
 def run(e, times, select_method='greedy'):
     b = Bendit(e)
@@ -82,7 +112,10 @@ def run(e, times, select_method='greedy'):
     for i in range(times):
         idx, is_best = b.select(select_method=select_method)
         used_count_list[idx] += 1
-        reward.append(b.act(idx))
+        r = b.act(idx)
+        reward.append(r)
+        if select_method == 'gba':
+            b.update_gba(idx, r)
         best.append(is_best)
     return reward, best
 
@@ -91,18 +124,15 @@ def main():
     count = 2000
     times = 1000
     # epsilon = [0.0, 0.01, 0.1]
-    epsilon = [0.1, -1]
+    epsilon = [0.1, -1, -1]
+    select_method = ['greedy', 'ucb', 'gba']
     reward_list = list()
     best_list = list()
-    for e in epsilon:
+    for i, e in enumerate(epsilon):
         a = list()
         b = list()
-        for i in range(count):
-            if e < 0:
-                select_method = 'ucb'
-            else:
-                select_method = 'greedy'
-            reward, best = run(e, times, select_method)
+        for j in range(count):
+            reward, best = run(e, times, select_method[i])
             a.append(reward)
             b.append(best)
         reward_list.append(a)
@@ -110,17 +140,17 @@ def main():
 
     ax = plt.subplot(2, 1, 1)
     for i, r in enumerate(reward_list):
-        plt.plot(range(times), np.mean(np.asarray(r), axis=0), label='e=%.2f'%epsilon[i])
+        plt.plot(range(times), np.mean(np.asarray(r), axis=0), label='method=%s'%select_method[i])
     plt.xlabel('steps')
     plt.ylabel('average reward')
     plt.legend(loc='upper left', frameon=False)
 
     ax2 = plt.subplot(2, 1, 2)
     for i, b in enumerate(best_list):
-        plt.plot(range(times), np.mean(np.asarray(b), axis=0), label='e=%.2f'%epsilon[i])
+        plt.plot(range(times), np.mean(np.asarray(b), axis=0), label='method=%s'%select_method[i])
     plt.xlabel('steps')
     plt.ylabel('% optimal choice')
-    plt.legend(loc='bottom right', frameon=False)
+    plt.legend(loc='lower right', frameon=False)
     plt.legend()
 
     # plt.savefig('./bendit.png')
