ad09a1e65b36bf3c82754f0a5c9d0dfcf0127d61
frank <603128423@qq.com>
2018-11-23 13:26:52 +0000

    lenet size

--- lenet/lenet.py
+++ lenet/lenet.py
@@ -3,40 +3,36 @@
 import numpy as np
 
 def convolute(input, filter):
-    '''
-    width == height
-    '''
-    w, _ = np.shape(input)
-    w1, _ = np.shape(filter)
+    w, h = np.shape(input)
+    w1, h1 = np.shape(filter)
     w2 = w - w1 + 1
-    output = np.zeros([w2, w2])
+    h2 = h - w1 + 1
+    output = np.zeros([w2, h2])
     for i in range(w2):
-        for j in range(w2):
-            tmp_input = input[i:w1+i, j:w1+j]
+        for j in range(h2):
+            tmp_input = input[i:w1+i, j:h1+j]
             output[i][j] = np.sum(tmp_input * filter)
     return output
 
 
-def max_sampling(input, size):
-    '''
-    width == height
-    size < width
-    '''
-    w, _ = np.shape(input)
-    w2 = w - size + 1
-    output = np.zeros([w2, w2])
+def max_pooling(input, size):
+    w, h = np.shape(input)
+    w1, h1 = size
+    w2 = w - w1 + 1
+    h2 = h - w1 + 1
+    output = np.zeros([w2, h2])
     for i in range(w2):
-        for j in range(w2):
-            tmp_input = input[i:size+i, j:size+j]
+        for j in range(h2):
+            tmp_input = input[i:w1+i, j:h1+j]
             output[i][j] = np.max(tmp_input)
     return output
 
 a = np.array([
-    [1, 2, 3, 4, 5],
-    [1, 0, 2, 3, 2],
-    [2, 0, 1, 0, 3],
-    [4, 1, 0, 2, 1],
-    [3, 1, 3, 2, 0]
+    [1, 2, 3, 4, 5, 3],
+    [1, 0, 2, 3, 2, 1],
+    [2, 0, 1, 0, 3, 3],
+    [4, 1, 0, 2, 1, 7],
+    [3, 1, 3, 2, 0, 2]
 ])
 
 b = np.array([
@@ -47,5 +43,5 @@ b = np.array([
 
 c = convolute(a, b)
 print(c)
-c = max_sampling(a, 3)
+c = max_pooling(a, [3, 3])
 print(c)
