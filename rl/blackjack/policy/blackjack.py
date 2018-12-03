a7159f0fcc991c1a869a2b67ff58edf13aa8cb72
FrankLeeC <frank@liwanyi.me>
2018-12-03 10:00:08 +0000

    blackjack

--- rl/blackjack/policy/blackjack.py
+++ rl/blackjack/policy/blackjack.py
@@ -46,6 +46,117 @@ Q = np.zeros([10, 10, 2, 2])  # player's sum, dealer's sum, usable ace, action
 # 第0表示STICK_ACTION, 第1表示HIT_ACTION
 POLICY = np.zeros([10, 10, 2, 2]) + 0.5  # initial propability. player's sum, dealer's sum, usable_ace, action 
 
+class State:
+
+    def __init__(self, play_sum, dealer_showing, usable_ace):
+        self.player_sum = play_sum
+        self.dealer_showing = dealer_showing
+        self.usable_ace = usable_ace
+        pass
+    
+    def player_sum(self):
+        '''
+        [12, 21]
+        '''
+        return self.play_sum
+
+    def dealer_showing(self):
+        '''
+        [2, 11]
+        '''
+        return self.dealer_showing
+
+    def usable_ace(self):
+        '''
+        0: nousable_ace
+        1: usable_ace
+        '''
+        return self.usable_ace
+
+
+class Episode:
+
+    def __init__(self):
+        self.states = []
+        self.action = []
+        self.reward = []
+
+    def add_state(self, state):
+        self.states.append(state)
+
+    def add_action(self, action):
+        self.action.append(action)
+
+    def add_reward(self, reward):
+        self.reward.append(reward)
+        
+    def states(self):
+        return self.states
+
+   def action(self):
+       return self.action
+
+    def reward(self):
+        return self.reward
+
+
+def random_start_state():
+    player_sum = random.randint(12, 21)
+    dealer_sum = random.randint(2, 11)
+    usable_ace = random.randint(0, 1)
+    return State(player_sum, dealer_showing, usable_ace)
+
+def get_action(state):
+    '''
+    STICK_ACTION 0
+    HIT_ACTION   1
+    '''
+    ps = state.player_sum() - 12
+    ds = state.dealer_showing() - 2
+    u = state.usable_ace()
+    p = POLICY[ps][ds][u]
+    if p[0] > p[1]:
+        return STICK_ACTION
+    return HIT_ACTION
+
+def generate_eposide():
+    episode = Episode()
+    state = random_start_state()
+    episode.add_state(state)
+    action = get_action(state)
+    episode.add_action(action)
+    # while action != STICK_ACTION:
+        
+    return episode
+
+def run():
+    count = 1000
+    for i in range(count):
+        eposide = generate_eposide()
+        states = set()
+        g = 0.0
+        for state in range(eposide):
+            key = '%d_%d_%d' % (state[0], state[1], state[2])
+            if key not in states:  # 当前episode未出现过该状态
+                g = 0.9 * g + state[3]
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Player:
 
     def __init__(self):
@@ -141,23 +252,3 @@ class Player:
                         POLICY[s][d][u][1] = 1.0
                         POLICY[s][d][u][0] = 0.0
 
-
-def generate_eposide():
-    '''
-    list of state
-    state: [player_sum, dealer_sum, usable_ace, value]
-    '''
-    pass
-
-def run():
-    count = 1000
-    for i in range(count):
-        eposide = generate_eposide()
-        states = set()
-        g = 0.0
-        for state in range(eposide):
-            key = '%d_%d_%d' % (state[0], state[1], state[2])
-            if key not in states:  # 当前episode未出现过该状态
-                g = 0.9 * g + state[3]
-        
-    pass
