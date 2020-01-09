d64efcbed33aa0e7135fa8b69157522352d22d0f
FrankLee <frank@liwanyi.me>
2020-01-09 22:00:05 +0800

    add

--- cnn/lenet/model.py
+++ cnn/lenet/model.py
@@ -16,34 +16,40 @@ POOLING_ECHO = 2  # 原样返回
 # matrix的层数与filters层数一致
 # matrix  单个正方形图层通道
 # filters 过滤器集合  可以多个过滤器
+# bias 偏移量 集合
 # stride filter 移动步长
 # padding 对齐方式
 # 返回与过滤器个数相同的卷积层
-def convolute2d(matrix, filters, stride, padding=PADDING_SAME):
+def convolute2d(matrix, filters, bias, stride, padding=PADDING_SAME):
     fsize, _ = filters[0].shape
     matrix, rounds = __padding(matrix, fsize, stride, padding)
     result = []
-    for _, f in enumerate(filters):
+    for i, f in enumerate(filters):
         f_width, _ = f.shape
         matrix.shape
-        d = [[__conv2d(matrix[r*stride:r*stride+f_width,c*stride:c*stride+f_width], f) for c in range(rounds)] for r in range(rounds)]
+        d = [[__conv2d(matrix[r*stride:r*stride+f_width,c*stride:c*stride+f_width], f, bias[i]) for c in range(rounds)] for r in range(rounds)]
         result.append(d)
     return np.array(result)
 
-def __conv2d(m, f):
-    return np.sum(m*f)
+def __conv2d(m, f, b):
+    return np.sum(m*f) + b
 
+# matrix 正方形图层通道 集合
+# fsize 窗口大小
+# bias 偏移量
 # round = [(n+2*p-f)//s]+1  round == out_width
 # PADDING_SAME    round = out_width = n  p = [s*(n-1)+f-n]/2
 # PADDING_VALID       p = 0   round == out_width = [(n-fsize)//stride]+1
-def pooling(data, fsize, stride, padding=PADDING_SAME, mode=POOLING_MAX):
-    data, rounds = __padding(data, fsize, stride, padding)
-    f = __max
-    if mode == POOLING_AVG:
-        f = __avg
-    if mode == POOLING_ECHO:
-        f = __echo
-    result = [[f(data[r*stride:r*stride+fsize,c*stride:c*stride+fsize]) for c in range(rounds)] for r in range(rounds)]
+def pooling(matrix, fsize, bias, stride, padding=PADDING_SAME, mode=POOLING_MAX):
+    result = []
+    for _, data in enumerate(matrix):
+        data, rounds = __padding(data, fsize, stride, padding)
+        f = __max
+        if mode == POOLING_AVG:
+            f = __avg
+        if mode == POOLING_ECHO:
+            f = __echo
+        result.append([[f(data[r*stride:r*stride+fsize,c*stride:c*stride+fsize])+bias for c in range(rounds)] for r in range(rounds)])
     return np.array(result)
 
 def __max(matrix):
@@ -69,3 +75,16 @@ def __padding(data, fsize, stride, padding=PADDING_SAME):
     result = np.zeros([pad_left + n + pad_right, pad_left + n + pad_right])
     result[pad_left:pad_left+n,pad_left:pad_left+n] = data
     return result, n
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def relu(x):
+    if x > 0:
+        return x
+    return 0
+
+def softmax(a):
+    m = np.max(a)
+    s = np.sum(np.exp(a - m))
+    return np.exp(a - m) / s
--- cnn/lenet/test.py
+++ cnn/lenet/test.py
@@ -12,13 +12,13 @@ def test_conv():
     print('---------')
     print(f)
     print('----------------')
-    c = model.convolute2d(a, f, 2, padding=model.PADDING_SAME)
+    c = model.convolute2d(a, f, np.repeat(0, f.shape[0]), 2, padding=model.PADDING_SAME)
     print(c.shape)
     print(c)
 
 def test_pooling():
     a = np.array([[1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2], [3, 3, 3, 3, 3, 3], [4, 4, 4, 4, 4, 4], [5, 5, 5, 5, 5, 5], [6, 6, 6, 6, 6, 6]])
-    b = model.pooling(a, 3, 2, padding=model.PADDING_SAME, mode=model.POOLING_MAX)
+    b = model.pooling([a], 3, 0, 2, padding=model.PADDING_SAME, mode=model.POOLING_MAX)
     print(b)
 
 
@@ -26,5 +26,5 @@ if __name__ == "__main__":
     # convolute()
     # image()
     # list_join()
-    test_conv()
-    # test_pooling()
+    # test_conv()
+    test_pooling()
